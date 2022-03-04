604f59b59c9045cf9874f5fb13cd6efc2b0441aa
rsaylor21 <99090657+rsaylor21@users.noreply.github.com>
2022-03-04 09:04:15 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+class Employee{
+	name:string;
+	title:string;
+	department:string
+	salary:number;
+
+constructor(employeeName(Steve):string, employeeTitle(Software Deveopler):string, employeeDepartment(Technology):string, employeeSalary(65000):number{
+	this.name=employeeName;
+	this.title=employeeTitle;
+	this.department=employeeDepartment;
+	this.salary=employeeSalary;
+}
+}
